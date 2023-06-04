46774a1d39bbc7d1ac8495b7ab2dd344ffaac882
meggouriIsmail <meggouri.ismail@gmail.com>
2023-06-04 14:05:29 +0100

    ebanking controllers

---
+++ ebanking-backend/src/main/java/com/meggo/ebankingbackend/controllers/CustomerRestController.java
@@ -0,0 +1,36 @@
+package com.meggo.ebankingbackend.controllers;
+
+import com.meggo.ebankingbackend.dtaos.CustomerDTO;
+import com.meggo.ebankingbackend.exceptions.CustomerNotFoundException;
+import com.meggo.ebankingbackend.services.BankAccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class CustomerRestController {
+    private BankAccountService bankAccountService;
+    @GetMapping("/customers")
+    public List<CustomerDTO> customers(){
+        return bankAccountService.listCustomers();
+    }
+    @GetMapping("/customers/{id}")
+    public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
+        return bankAccountService.getCustomer(customerId);
+    }
+    @PostMapping("/customers")
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
+        return bankAccountService.saveCustomer(customerDTO);
+    }
+    @PutMapping("/customers/{customerId}")
+    public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO){
+        customerDTO.setId(customerId);
+        return bankAccountService.updateCustomer(customerDTO);
+    }
+    @DeleteMapping("/customers/{id}")
+    public void deleteCustomer(@PathVariable Long id){
+        bankAccountService.deleteCustomer(id);
+    }
+}

4d668ef861b1b1570b3d354c65c22122e961d8a6
meggouriIsmail <meggouri.ismail@gmail.com>
2023-06-21 15:06:46 +0100

    big-data exam

--- SparkAeroport/src/main/java/org/meggouri/Main.java
+++ SparkAeroport/src/main/java/org/meggouri/Main.java
@@ -1,7 +1,53 @@
 package org.meggouri;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SparkSession spark = SparkSession.builder()
+                .appName("Main")
+                .master("local[*]")
+                .getOrCreate();
+
+        Dataset<Row> volsDF = spark.read()
+                .format("jdbc")
+                .option("url", "jdbc:mysql://localhost:3306/db_aer")
+                .option("dbtable", "vols")
+                .option("user", "root")
+                .option("password", "")
+                .load();
+
+        Dataset<Row> reservationsDF = spark.read()
+                .format("jdbc")
+                .option("url", "jdbc:mysql://localhost:3306/db_aer")
+                .option("dbtable", "reservations")
+                .option("user", "root")
+                .option("password", "")
+                .load();
+
+        Dataset<Row> passagersDF = spark.read()
+                .format("jdbc")
+                .option("url", "jdbc:mysql://localhost:3306/db_aer")
+                .option("dbtable", "passangers")
+                .option("user", "root")
+                .option("password", "")
+                .load();
+
+        volsDF.createOrReplaceTempView("vols");
+        reservationsDF.createOrReplaceTempView("reservations");
+        passagersDF.createOrReplaceTempView("passagers");
+
+        Dataset<Row> result = spark.sql(
+                "SELECT v.Id AS ID_VOL, v.Date_Depart, COUNT(DISTINCT r.Id_passanger) AS NOMBRE " +
+                        "FROM vols v " +
+                        "LEFT JOIN reservations r ON v.Id = r.Id_Vol " +
+                        "GROUP BY v.Id, v.Date_Depart"
+        );
+
+        result.show();
+
+        spark.stop();
     }
 }

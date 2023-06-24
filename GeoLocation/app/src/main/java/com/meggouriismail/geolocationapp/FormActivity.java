621d52a4572c78436449f35ffea135dbe554f74a
meggouriIsmail <meggouri.ismail@gmail.com>
2023-06-24 20:05:07 +0100

    get location from ip_address

--- GeoLocation/app/src/main/java/com/meggouriismail/geolocationapp/FormActivity.java
+++ GeoLocation/app/src/main/java/com/meggouriismail/geolocationapp/FormActivity.java
@@ -1,14 +1,53 @@
 package com.meggouriismail.geolocationapp;
 
-
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class FormActivity extends AppCompatActivity {
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "https://ipinfo.io/170.186.160.93/geo";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_form);
+
+
+        getData();
+    }
+
+    private void getData() {
+        // RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        // String Request initialized
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(getApplicationContext(), "Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
     }
 }
